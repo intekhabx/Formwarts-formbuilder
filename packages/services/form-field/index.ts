9c5e8b0781794ce9efd08e8e440829058210b4c3
itz-devilx <intekhab118211989@gmail.com>
2026-05-31 03:05:03 +0530

    feat: add form_field services and type and validate their input

---
+++ packages/services/form-field/index.ts
@@ -0,0 +1,109 @@
+import db, { eq, max } from "@repo/database";
+import {formFieldTable} from '@repo/database/models/form-field'
+import { createFormFieldInput, CreateFormFieldInputType, deleteFormFieldInput, DeleteFormFieldInputType, getFormFieldInput, GetFormFieldInputType, updateFormFieldInput, UpdateFormFieldInputType } from "./model";
+
+
+
+function makeLabelKey(label: string): string {
+  return label.toLowerCase().trim().replace(/[^a-z0-9]+/g, "_").replace(/^_|_$/g, '');
+}
+
+export default class FormFieldService {
+
+  //fractional indexing
+  private async getNextIndex(formId: string): Promise<string>{
+    const result = await db.select({maxIndex: max(formFieldTable.index)}).from(formFieldTable).where(eq(formFieldTable.formId, formId));
+
+    const current = result[0]?.maxIndex;
+    const next = current ? parseFloat(current) + 1 : 1;
+    return next.toFixed(2);
+  }
+
+
+
+  public async createFormField(payload: CreateFormFieldInputType){
+    const {formId, isRequired, label, type, description, placeholder} = await createFormFieldInput.parseAsync(payload);
+
+    const labelKey = makeLabelKey(label);
+    const index = await this.getNextIndex(formId);
+
+    const result = await db.insert(formFieldTable).values({
+      label,
+      labelKey,
+      index,
+      type,
+      formId,
+      description,
+      isRequired,
+      placeholder,
+    }).returning({id: formFieldTable.id})
+
+    if(!result || result.length === 0 || !result[0]?.id){
+      throw new Error("something went wrong while creating the field");
+    }
+
+    return {id: result[0].id, index, labelKey}
+  }
+
+
+  public async updateFormField(payload: UpdateFormFieldInputType){
+    const {fieldId, ...updates} = await updateFormFieldInput.parseAsync(payload);
+
+    const patch: Partial<typeof formFieldTable.$inferInsert> = {};
+
+    if(updates.label !== undefined) patch.label = updates.label;
+    if(updates.isRequired !== undefined) patch.isRequired = updates.isRequired;
+    if(updates.type !== undefined) patch.type = updates.type;
+    if('description' in updates) patch.description = updates.description ?? null;
+    if('placeholder' in updates) patch.placeholder = updates.placeholder ?? null;
+
+    if(Object.keys(patch).length === 0){
+      throw new Error("no fields provided to update");
+    }
+
+    const result = await db.update(formFieldTable).set(patch)
+                  .where(eq(formFieldTable.id, fieldId)).returning({id: formFieldTable.id});
+
+    if(!result || result.length === 0 ){
+      throw new Error(`field with id ${fieldId} doesn't exist`);
+    }
+
+    return {id: result[0]!.id}
+  }
+
+
+  public async deleteFormField(payload: DeleteFormFieldInputType){
+    const {fieldId} = await deleteFormFieldInput.parseAsync(payload);
+
+    const result = await db.delete(formFieldTable).where(eq(formFieldTable.id, fieldId)).returning({id: formFieldTable.id});
+
+    if(!result || result.length === 0){
+      throw new Error(`field with id ${fieldId} doesn't exist`);
+    }
+
+    return {id: result[0]!.id}
+  }
+
+
+  public async getFormField(payload: GetFormFieldInputType){
+    const {formId} = await getFormFieldInput.parseAsync(payload);
+
+    const result = await db.select({
+      id: formFieldTable.id,
+      label: formFieldTable.label,
+      description: formFieldTable.description,
+      type: formFieldTable.type,
+      isRequired: formFieldTable.isRequired,
+      placeholder: formFieldTable.placeholder,
+      formId: formFieldTable.formId,
+      index: formFieldTable.index
+    }).from(formFieldTable).where(eq(formFieldTable.formId, formId));
+
+    if(!result || result.length === 0){
+      throw new Error(`form with this id ${formId} doesn't exist`)
+    }
+
+    return {form: result[0]}
+  }
+
+}
---
+++ packages/services/form-field/model.ts
@@ -0,0 +1,43 @@
+import {z} from 'zod';
+
+
+const fieldTypeEnum = z.enum(["TEXT", "NUMBER", "EMAIL", "YES_NO", "PASSWORD"]);
+
+export const createFormFieldInput = z.object({
+  label: z.string().max(100).describe("display label of the form field"),
+  type: fieldTypeEnum.describe("type of the field"),
+  formId: z.uuid().describe("UUID of the form this field belongs to"),
+  placeholder: z.string().optional().describe("placholder text of the form field"),
+  description: z.string().optional().describe("description of the form field"),
+  isRequired: z.boolean().optional().default(false).describe("field is required or not"),
+})
+
+export type CreateFormFieldInputType = z.infer<typeof createFormFieldInput>;
+
+
+export const updateFormFieldInput = z.object({
+  fieldId: z.uuid().describe("UUID of the form this field belongs to"),
+  label: z.string().max(100).optional().describe("updated display labe;"),
+  type: fieldTypeEnum.optional().describe("updated type of the field"),
+  placeholder: z.string().optional().nullable().describe("updated placholder text of the form field"),
+  description: z.string().optional().nullable().describe("updated description of the form field"),
+  isRequired: z.boolean().optional().describe("updated required flag"),
+})
+
+export type UpdateFormFieldInputType = z.infer<typeof updateFormFieldInput>;
+
+
+
+export const deleteFormFieldInput = z.object({
+  fieldId: z.uuid().describe("UUID of the form field to delete")
+})
+
+export type DeleteFormFieldInputType = z.infer<typeof deleteFormFieldInput>;
+
+
+
+export const getFormFieldInput = z.object({
+  formId: z.uuid().describe("UUID of the form want to fetch")
+})
+
+export type GetFormFieldInputType = z.infer<typeof getFormFieldInput>;
