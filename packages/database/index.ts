6f5827f1cecc5251379ab9d1dd57a3dd7633c295
itz-devilx <intekhab118211989@gmail.com>
2026-05-31 13:40:50 +0530

    feat: neon DB setup and configuration completed

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,7 +1,11 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+// import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "./env";
+import { neon } from "@neondatabase/serverless";
 
-export const db = drizzle(env.DATABASE_URL);
+// export const db = drizzle(env.DATABASE_URL);
+const sql = neon(env.DATABASE_URL);
+export const db = drizzle(sql);
 export * from "drizzle-orm";
 export default db;
